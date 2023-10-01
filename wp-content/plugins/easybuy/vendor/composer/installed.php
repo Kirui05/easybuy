609297ad1ea464f029f18f2452383d1b43e14c29
unknown <nicholaskirui6@gmail.com>
2023-10-02 00:02:57 +0300

    plugin

---
+++ wp-content/plugins/easybuy/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'kipyegon/easybuy',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'dd6eb5f6eada6d59d721f0006ae4fcf14845ce6c',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'kipyegon/easybuy' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'dd6eb5f6eada6d59d721f0006ae4fcf14845ce6c',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
